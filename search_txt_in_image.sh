4240868d9b5d18f61beb1cfb38e29a8b0cb741df
Zhilong Jia <zhilongjia@gmail.com>
2021-09-09 11:06:53 +0800

    add mktemp

--- search_txt_in_image.sh
+++ search_txt_in_image.sh
@@ -24,18 +24,24 @@ while getopts ":d:t:ch" opt; do
     h )
       echo "Usage:"
       echo "search_txt_in_image.sh"
-      echo "seach txt in images after OCR the images with -d parameter."
+      echo "========================================================"
       echo "-d the dir of figures (should be full paths)"
       echo "-s the searching target."
       echo "-t topN images to show"
       echo "-c CASE sensitive. Donot use it if wanting IGNORECASE."
       echo "========================================================"
+      echo "seach txt in images after OCR the images with -d parameter."
       echo "The database of the text from image is located ~/.search_txt_in_image.db "
       echo "tesseract, fim should be installed in advance."
       echo "========================================================"
       echo "examples:"
-      echo "search_txt_in_image.sh -d ~/Picutres "cohort" "
+      echo "search_txt_in_image.sh -d "~/Picutres" "cohort" "
+      echo "search_txt_in_image.sh -d "~/Picutres ~/pngs" "cohort" "
       echo "search_txt_in_image.sh cohort"
+      echo "search_txt_in_image.sh 'cohort|gene' "
+      echo "========================================================"
+      echo "author: zhilongjia@qq.com"
+      echo "9 Sept. 2021"
       exit 0  
       ;; 
   esac
@@ -51,13 +57,14 @@ target=$1
 
 
 ##############################################################################################################
-echo "Starting OCR images..."
+
 # db
 search_txt_in_image_db=~/.search_txt_in_image.db
 
 # build the db if -d parameter exist
 if [[ -v dir ]]
 then
+    echo "Starting OCR images..."
     imgs=`find $dir -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}'`
     
     # the number of figures
@@ -66,25 +73,41 @@ then
     # echo $line
     line_i=1
     
+    temp_figure_file=$(mktemp)
     # touch ${search_txt_in_image_db}
     for img in ${imgs}
     do
         printf "$line_i / $line :  ${img}\n"
 
-        printf "${img}\t" >> ${search_txt_in_image_db}
-        tesseract ${img} - -l eng+chi_sim --oem 1  | tr  '\n' '\t' >> ${search_txt_in_image_db}
-        echo "\n" >> ${search_txt_in_image_db}
+        printf "${img}\t" >> ${temp_figure_file}
+        tesseract ${img} - -l eng+chi_sim --oem 3  | tr  '\n' '\t'| tr -d "[:blank:]" >> ${temp_figure_file}
+        echo "\n" >> ${temp_figure_file}
         ((line_i++))
     done
     
+
+    cat ${temp_figure_file} >> ${search_txt_in_image_db}
+    rm ${temp_figure_file}
+
     # deduplicates
     sort -u ${search_txt_in_image_db} -o ${search_txt_in_image_db}
-
+    
 fi
 
-echo " #figures: "`wc -l ${search_txt_in_image_db}`
+echo " # figures: "`wc -l ${search_txt_in_image_db}`
 
 ##############################################################################################################
 # search and show your result.
-awk -v target="$target" -v ignorecase="${ignorecase=1}"  'BEGIN{IGNORECASE=ignorecase}$0 ~ target{print $1}' ${search_txt_in_image_db} | head -${topN=5} | fim -
+# use mktemp so the ouput can be cat and view
 
+if [[ -v target ]]
+then
+    temp_result_file=$(mktemp)
+
+    awk -v target="$target" -v ignorecase="${ignorecase=1}"  'BEGIN{IGNORECASE=ignorecase}$0 ~ target{print $1}' ${search_txt_in_image_db} > ${temp_result_file}
+
+    cat ${temp_result_file}
+    head -${topN=5} ${temp_result_file} | fim -
+
+    rm ${temp_result_file}
+fi
